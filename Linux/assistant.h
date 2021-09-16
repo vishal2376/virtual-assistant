fe3cd40e14b06aed839543043e0881fc1b00766b
Vishal Singh <vishal@localhost.localdomain>
2021-09-16 19:29:55 +0530

    linux setup

---
+++ Linux/assistant.h
@@ -0,0 +1,44 @@
+/*
+----------------------------------------------------------------------------
+               Project title   : Hydron(virtual assistant)
+               Made By         : Vishal Singh
+               Written in      : C++
+               Started project : 27 December , 2018
+               Platform        : Windows
+               IDE used        : Codeblocks
+               Last Modified   : 20 December , 2020
+               Total Code      : 700+ Lines
+ ----------------------------------------------------------------------------
+*/
+
+
+
+#ifndef ASSISTANT_H
+#define ASSISTANT_H
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <stdio.h>
+#include <cstring>
+#include <unistd.h>
+#include <ctype.h>
+#include <ctime>
+
+using namespace std;
+
+class assistant
+{
+
+public:
+  assistant();
+  ~assistant();
+  void greeting();
+  void typing(string);
+  void clock();
+  void speak(string);
+
+};
+
+#endif // ASSISTANT_H
